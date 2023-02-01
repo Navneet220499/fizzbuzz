b31c40fcb4903feeaa0920f0a4253af741d56ce2
Navneet <nv424169@dal.ca>
2023-01-31 20:17:04 -0500

    task1 initial commit

---
+++ task1.py
@@ -0,0 +1,7 @@
+def l33t(text):
+    text = text.replace("o","0").replace("O","0")
+    text = text.replace("a","4").replace("A","4")
+    text = text.replace("e","3").replace("E","3")
+    text = text.replace("i","1").replace("I","1")
+    text = text.replace("-er","-xor").replace("-er","-zor")
+    
